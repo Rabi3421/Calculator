f6ea07b249365174bbf6c38590db5addc98c1f46
Rabi3421 <rabinarayan3421@gmail.com>
2022-08-19 00:00:16 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,184 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton";
+import { useReducer } from "react";
+
+export const ACTION = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVELUATE: "evaluate",
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTION.ADD_DIGIT:
+      if(state.overwrite){
+        return {
+          ...state,
+          currentOperand:payload.digit,
+          overwrite:false, 
+        }
+      }
+      if (payload.digit === "0" && state.currentOperand === "0") {
+        return state;
+      }
+      if (payload.digit === "." && state.currentOperand.includes(".")) {
+        return state;
+      }
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      };
+
+    case ACTION.CHOOSE_OPERATION:
+      if (
+        state.currentOperand === undefined &&
+        state.previousOperand === undefined
+      ) {
+        return state;
+      }
+      if (state.currentOperand === undefined) {
+        return {
+          ...state,
+          operation: payload.operation,
+        };
+      }
+      if (state.previousOperand === undefined) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: undefined,
+        };
+      }
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: undefined,
+      };
+
+    case ACTION.CLEAR:
+      return {};
+    
+    case ACTION.DELETE_DIGIT:
+      if(state.overwrite){
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: undefined,
+
+        }
+      }
+      if(state.currentOperand === undefined){
+        return state
+      }
+      if(state.currentOperand.length === 1){
+        return {...state,currentOperand : undefined}
+      }
+      return {
+        ...state,
+        currentOperand : state.currentOperand.slice(0, -1)
+      }
+       
+    case ACTION.EVELUATE:
+      if(state.operation === undefined || state.currentOperand === undefined || state.previousOperand === undefined){
+        return state
+      }
+      return {
+        ...state,
+        overwrite:true,
+        previousOperand : undefined,
+        operation : undefined,
+        currentOperand : evaluate(state),
+      }
+  }
+}
+
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(current)) {
+    return "";
+  }
+  let computation = "";
+  switch (operation) {
+    case "+":
+      computation = prev + current;
+      break;
+    case "-":
+      computation = prev - current;
+      break;
+    case "*":
+      computation = prev * current;
+      break;
+    case "/":
+      computation = prev / current;
+      break;
+  }
+  return computation.toString();
+}
+
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us",{
+  maximumFractionDigits: 0,
+})
+function formatOperand(operand){
+  const [integer,decimal] = operand.split(".")
+  if(operand === undefined){
+    return
+  }
+  if(decimal === undefined){
+    INTEGER_FORMATTER.format(integer)
+  }
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+}
 
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">
+          {previousOperand}
+          {operation}
+        </div>
+        <div className="current-operand">{currentOperand}</div>
+      </div>
+      <button
+        className="span-two"
+        style={{ backgroundColor: "red" }}
+        onClick={() => dispatch({ type: ACTION.CLEAR })}
+      >
+        AC
+      </button>
+      <button onClick={() => dispatch({ type: ACTION.DELETE_DIGIT })}>DEL</button>
+      <OperationButton operation="/" dispatch={dispatch} />
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
+      <button
+        className="span-two"
+        style={{ backgroundColor: "green" }}
+        onClick={() => dispatch({ type: ACTION.EVELUATE })}
+      >
+        =
+      </button>
     </div>
   );
 }
---
+++ src/OperationButton.js
@@ -0,0 +1,11 @@
+import { ACTION } from "./App";
+
+export default function OperationButton({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTION.CHOOSE_OPERATION, payload: { operation } })}
+    >
+      {operation}
+    </button>
+  );
+}
